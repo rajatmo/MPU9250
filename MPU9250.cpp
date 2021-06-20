349f2ca43467f63f1a793a4560e1d4325ca1aa65
Rajat Mandal <32079339+rajatmo@users.noreply.github.com>
2021-06-20 23:32:09 +0530

    Update MPU9250.cpp

---
+++ MPU9250.cpp
@@ -0,0 +1,216 @@
+#include "MPU9250.h"
+#include <cstdint>
+
+//Constructor
+MPU9250::MPU9250(I2C &bus,uint8_t address){
+  _i2c = &bus; // I2C bus
+  _address = address; // I2C address
+}
+
+//Reading registers-helper function, returns 0 on success
+int MPU9250::readRegisters(uint8_t subAddress, uint8_t count, uint8_t* dest){
+    uint8_t result;
+    char reg_out[1];
+
+    reg_out[0] = subAddress;
+    result = _i2c->write(_address, reg_out, 1, true);
+    result |= _i2c->read(_address, (char* )dest, count, false);
+    return result;
+}
+
+int MPU9250::writeRegister(uint8_t subAddress, uint8_t data) {
+    char buf[2] = {(char)subAddress, (char)data};
+    uint8_t chk[1];
+    int result;
+
+    result = _i2c->write(_address, buf, 2);
+    ThisThread::sleep_for(10ms);
+    readRegisters(subAddress, 1, chk);
+    if((char)chk[0] == buf[1]){
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+/* gets the MPU9250 WHO_AM_I register value, expected to be 0x71 */
+int MPU9250::whoAmI(){
+  // read the WHO AM I register
+  if (readRegisters(WHO_AM_I, 1, _buffer) < 0) {
+    return -1;
+  }
+  // return the register value
+  return _buffer[0];
+}
+
+int MPU9250::readAK8963Registers(uint8_t subAddress, uint8_t count, uint8_t* dest){
+  // set slave 0 to the AK8963 and set for read
+	if (writeRegister(I2C_SLV0_ADDR,AK8963_I2C_ADDR | I2C_READ_FLAG) < 0) {
+    return -1;
+  }
+  // set the register to the desired AK8963 sub address
+	if (writeRegister(I2C_SLV0_REG,subAddress) < 0) {
+    return -2;
+  }
+  // enable I2C and request the bytes
+	if (writeRegister(I2C_SLV0_CTRL,I2C_SLV0_EN | count) < 0) {
+    return -3;
+  }
+	//ThisThread::sleep_for(1ms); // takes some time for these registers to fill
+  // read the bytes off the MPU9250 EXT_SENS_DATA registers
+	_status = readRegisters(EXT_SENS_DATA_00,count,dest); 
+  return _status;
+}
+
+/* writes a register to the AK8963 given a register address and data */
+int MPU9250::writeAK8963Register(uint8_t subAddress, uint8_t data){
+  // set slave 0 to the AK8963 and set for write
+	if (writeRegister(I2C_SLV0_ADDR,AK8963_I2C_ADDR) < 0) {
+    return -1;
+  }
+  // set the register to the desired AK8963 sub address 
+	if (writeRegister(I2C_SLV0_REG,subAddress) < 0) {
+    return -2;
+  }
+  // store the data for write
+	if (writeRegister(I2C_SLV0_DO,data) < 0) {
+    return -3;
+  }
+  // enable I2C and send 1 byte
+	if (writeRegister(I2C_SLV0_CTRL,I2C_SLV0_EN | (uint8_t)1) < 0) {
+    return -4;
+  }
+	// read the register and confirm
+	if (readAK8963Registers(subAddress,1,_buffer) < 0) {
+    return -5;
+  }
+	if(_buffer[0] == data) {
+  	return 1;
+  } else{
+  	return -6;
+  }
+}
+
+/* starts communication with the MPU-9250 */
+int MPU9250::begin(){
+     // using I2C for communication
+    // starting the I2C bus
+  // select clock source to gyro
+  if(writeRegister(PWR_MGMNT_1,CLOCK_SEL_PLL) < 0){
+    return -1;
+  }
+  // enable I2C master mode
+  if(writeRegister(USER_CTRL,I2C_MST_EN) < 0){
+    return -2;
+  }
+  // set the I2C bus speed to 400 kHz
+  if(writeRegister(I2C_MST_CTRL,I2C_MST_CLK) < 0){
+    return -3;
+  }
+  // set AK8963 to Power Down
+  writeAK8963Register(AK8963_CNTL1,AK8963_PWR_DOWN);
+  // reset the MPU9250
+  writeRegister(PWR_MGMNT_1,PWR_RESET);
+  // wait for MPU-9250 to come back up
+  ThisThread::sleep_for(1ms);
+  // reset the AK8963
+  writeAK8963Register(AK8963_CNTL2,AK8963_RESET);
+  // select clock source to gyro
+  if(writeRegister(PWR_MGMNT_1,CLOCK_SEL_PLL) < 0){
+    return -4;
+  }
+  // check the WHO AM I byte, expected value is 0x71 (decimal 113) or 0x73 (decimal 115)
+  if((whoAmI() != 113)&&(whoAmI() != 115)){
+    return -5;
+  }
+  // enable accelerometer and gyro
+  if(writeRegister(PWR_MGMNT_2,SEN_ENABLE) < 0){
+    return -6;
+  }
+  // setting accel range to 16G as default
+  if(writeRegister(ACCEL_CONFIG,ACCEL_FS_SEL_16G) < 0){
+    return -7;
+  }
+  _accelScale = G * 16.0f/32767.5f; // setting the accel scale to 16G
+  _accelRange = ACCEL_RANGE_16G;
+  // setting the gyro range to 2000DPS as default
+  if(writeRegister(GYRO_CONFIG,GYRO_FS_SEL_2000DPS) < 0){
+    return -8;
+  }
+  _gyroScale = 2000.0f/32767.5f * _d2r; // setting the gyro scale to 2000DPS
+  _gyroRange = GYRO_RANGE_2000DPS;
+  // setting bandwidth to 184Hz as default
+  if(writeRegister(ACCEL_CONFIG2,ACCEL_DLPF_184) < 0){ 
+    return -9;
+  } 
+  if(writeRegister(CONFIG,GYRO_DLPF_184) < 0){ // setting gyro bandwidth to 184Hz
+    return -10;
+  }
+  _bandwidth = DLPF_BANDWIDTH_184HZ;
+  // setting the sample rate divider to 0 as default
+  if(writeRegister(SMPDIV,0x00) < 0){ 
+    return -11;
+  } 
+  _srd = 0;
+  // enable I2C master mode
+  if(writeRegister(USER_CTRL,I2C_MST_EN) < 0){
+  	return -12;
+  }
+	// set the I2C bus speed to 400 kHz
+	if( writeRegister(I2C_MST_CTRL,I2C_MST_CLK) < 0){
+		return -13;
+	}
+	// check AK8963 WHO AM I register, expected value is 0x48 (decimal 72)
+	if( whoAmIAK8963() != 72 ){
+    return -14;
+	}
+  /* get the magnetometer calibration */
+  // set AK8963 to Power Down
+  if(writeAK8963Register(AK8963_CNTL1,AK8963_PWR_DOWN) < 0){
+    return -15;
+  }
+  ThisThread::sleep_for(100ms); // long wait between AK8963 mode changes
+  // set AK8963 to FUSE ROM access
+  if(writeAK8963Register(AK8963_CNTL1,AK8963_FUSE_ROM) < 0){
+    return -16;
+  }
+  ThisThread::sleep_for(100ms); // long wait between AK8963 mode changes
+  // read the AK8963 ASA registers and compute magnetometer scale factors
+  readAK8963Registers(AK8963_ASA,3,_buffer);
+  _magScaleX = ((((float)_buffer[0]) - 128.0f)/(256.0f) + 1.0f) * 4912.0f / 32760.0f; // micro Tesla
+  _magScaleY = ((((float)_buffer[1]) - 128.0f)/(256.0f) + 1.0f) * 4912.0f / 32760.0f; // micro Tesla
+  _magScaleZ = ((((float)_buffer[2]) - 128.0f)/(256.0f) + 1.0f) * 4912.0f / 32760.0f; // micro Tesla 
+  // set AK8963 to Power Down
+  if(writeAK8963Register(AK8963_CNTL1,AK8963_PWR_DOWN) < 0){
+    return -17;
+  }
+  ThisThread::sleep_for(100ms); // long wait between AK8963 mode changes  
+  // set AK8963 to 16 bit resolution, 100 Hz update rate
+  if(writeAK8963Register(AK8963_CNTL1,AK8963_CNT_MEAS2) < 0){
+    return -18;
+  }
+  ThisThread::sleep_for(100ms); // long wait between AK8963 mode changes
+  // select clock source to gyro
+  if(writeRegister(PWR_MGMNT_1,CLOCK_SEL_PLL) < 0){
+    return -19;
+  }       
+  // instruct the MPU9250 to get 7 bytes of data from the AK8963 at the sample rate
+  readAK8963Registers(AK8963_HXL,7,_buffer);
+  // estimate gyro bias
+  //if (calibrateGyro() < 0) {
+  //  return -20;
+  //}
+  // successful init, return 1
+  return 1;
+}
+
+
+int MPU9250::whoAmIAK8963(){
+  // read the WHO AM I register
+  if (readAK8963Registers(AK8963_WHO_AM_I,1,_buffer) < 0) {
+    return -1;
+  }
+  // return the register value
+  return _buffer[0];
+}
